524fc843b51dff7921a36118e6514c96a19ce4b3
dasssseul <kds0339@naver.com>
2021-04-18 13:49:43 +0900

    update dfs, bfs

--- chapter05_dfs,bfs.py
+++ chapter05_dfs,bfs.py
@@ -83,5 +83,159 @@ def gcd(a, b):
 print(gcd(192, 162))
 
 
+# DFS 소스코드 예제
+
+# 각 노드가 연결된 정보를 표현 (2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 표현 (1차원 리스트)
+visited = [False] * 9
+
+# DFS 메서드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
+
+
+# BFS 소스 코드 예제
+
+from collections import deque
+
+# 각 노드가 연결된 정보를 표현 (2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 표현 (1차원 리스트)
+visited = [False] * 9
+
+# BFS 메서드 정의
+
+def bfs(graph, start, visited):
+    # 큐(queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력하기
+        v = queue.popleft()
+        print(v, end=' ')
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 정의된 BFS 함수 호출
+bfs(graph, 1, visited)
+
+
+# 실전 문제1. 음료수 얼려 먹기
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int,input())))
+
+# DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+
+def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+        return True
+    return False
+
+# 모든 노드(위치)에 대하여 음료수 채우기
+
+icecream = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            icecream += 1
+
+print(icecream)
+
+
+# 실전 문제4. 미로 탈출
+
+from collections import deque
+
+n, m = map(int, input().split())
+
+miro = []
+for i in range(n):
+    miro.append(list(map(int, input())))
+
+# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 소스코드 구현
+def bfs(x, y):
+    # 큐(queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))
+    # 큐가 빌 때까지 반복하기
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 4가지 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 괴물이 존재하는 경우 무시
+            if miro[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if miro[nx][ny] == 1:
+                miro[nx][ny] = miro[x][y] + 1
+                queue.append((nx, ny))
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return miro[n-1][m-1]
+
+print(bfs(0,0))
+
+
 
 
