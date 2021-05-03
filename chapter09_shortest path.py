e85ffa78561d970723b7b0c08aa2c8e975eb9b1f
dasssseul <kds0339@naver.com>
2021-05-03 12:06:40 +0900

    update shortest path

--- chapter09_shortest path.py
+++ chapter09_shortest path.py
@@ -2,7 +2,6 @@
 # [이코테 2021 강의 몰아보기]
 # chapter09_shortest path
 
-
 # 다익스트라 알고리즘 : 간단한 구현 방법
 
 import sys
@@ -88,7 +87,6 @@ result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result)
 
 
-
 # 힙 라이브러리 사용 예제: 최대 힙
 
 import heapq
@@ -107,7 +105,6 @@ def heapsort(iterable):
 
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result)
-
 
 
 # 다익스트라 알고리즘 : 개선된 구현 방법
@@ -167,5 +164,132 @@ for i in range(1, n+1):
 
 
 
-        
-        
+# 플로이드 워셜 알고리즘
+
+# 무한을 의미하는 값으로 10억을 설정
+INF = int(1e9)
+
+# 노드의 개수 및 간선의 개수를 입력받기
+n = int(input())
+m = int(input())
+
+# 2차원 리스트(그래프 표현)를 만들고, 무한으로 초기화
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용은 C라고 설정
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        # 도달할 수 없는 경우, 무한(INFINITY)이라고 풀력
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        # 도달할 수 있는 경우 거리를 출력
+        else:
+            print(graph[a][b], end=" ")
+    print()
+
+
+
+# 실전 문제1. 전보
+
+# 다익스트라 알고리즘 이용
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, start = map(int, input().split())
+
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q,(0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+# 도달할 수 있는 노드의 개수
+count = 0
+# 도달할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
+max_distance = 0
+
+for d in distance:
+    if d != 1e9:
+        count += 1
+        max_distance = max(max_distance, d)
+
+# 시작 노드는 제외해야하므로 count-1을 출력
+print(count-1, max_distance)
+
+
+
+# 실전 문제2. 미래 도시
+
+# 플로이드 워셜 알고리즘 이용
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
+
+
+
